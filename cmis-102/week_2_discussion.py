9835b5ae1117f3e74797faf4c864231cb7d12ddc
Sean Gorman <85382796+scgorman@users.noreply.github.com>
2022-08-15 16:36:07 -0600

    copied week_2_discussion.py from online IDE
    Needs comments

---
+++ cmis-102/week_2_discussion.py
@@ -0,0 +1,9 @@
+# Add comments
+prompt_0 = 'What is your first number?'
+num_0 eval(input(prompt_0))
+
+prompt_1 = 'What is your second number?'
+num_1 eval(input(prompt_1))
+
+result = num_0 + num_1
+print('The result of adding both numbers together is: ' + str(result))
