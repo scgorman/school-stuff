fc9b621dd1b58074b63e42ff933df50fa6aacfcf
Sean Gorman <85382796+scgorman@users.noreply.github.com>
2022-08-15 16:45:46 -0600

    update week_2_discussion.py
    i think it's done

--- cmis-102/week_2_discussion.py
+++ cmis-102/week_2_discussion.py
@@ -1,13 +1,13 @@
 # Prompt the user for their first number and assign it to a variable
 prompt_0 = 'What is your first number?'
-num_0 eval(input(prompt_0))
+num_0 = eval(input(prompt_0))
 
 # Prompt the user for their second number and assign it to a variable
 prompt_1 = 'What is your second number?'
-num_1 eval(input(prompt_1))
+num_1 = eval(input(prompt_1))
 
 # Add both number together and assign it to the variable: result
 result = num_0 + num_1
 
 # Print the result along with a string explaining it
-print('The result of adding ' + str(num_0) + ' and ' + str(num_0) + ' is: ' str(result))
+print('The result of adding those numbers together is: ' + str(result))
